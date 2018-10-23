452b7ca13c19bfd661fe5f8df0c2b7bdd74b9765
mojumah <jumah.mohammad@gmail.com>
2018-10-23 22:50:10 +0100

    script to install docker on vagrant vm, and then start jenkins in a container

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo docker run -u root --rm -d -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean
